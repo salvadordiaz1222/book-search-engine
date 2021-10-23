94e7de7a32dca61b65ae0c307234db62a84185d9
salvadordiaz1222 <salvador.diaz1222@gmail.com>
2021-10-23 10:25:29 -0500

    added the following mutations LOGIN USER ADD USER SAVE BOOK and REMOVE BOOK

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,54 @@
+// LOGIN_USER will execute the loginUser mutation set up using Apollo Server.
+// ADD_USER will execute the addUser mutation.
+// SAVE_BOOK will execute the saveBook mutation.
+// REMOVE_BOOK will execute the removeBook mutation.
+
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($booksInfo: SaveBookInput!) {
+    saveBook(booksInfo: $booksInfo) {
+      bookCount
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      bookCount
+      savedBooks {
+        title
+        image
+        description
+        authors
+        link
+        bookId
+      }
+    }
+  }
+`;
