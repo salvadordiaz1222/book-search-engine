ee8bbe2c81773373e58f0527f11ef5e2d2129234
salvadordiaz1222 <salvador.diaz1222@gmail.com>
2021-10-23 10:27:14 -0500

    added the query me

---
+++ client/src/utils/queries.js
@@ -0,0 +1,22 @@
+// This will hold the query GET_ME, which will execute the me query set up using Apollo Server.
+
+import { gql } from "@apollo/client";
+
+export const GET_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        title
+        image
+        description
+        authors
+        link
+        bookId
+      }
+    }
+  }
+`;
